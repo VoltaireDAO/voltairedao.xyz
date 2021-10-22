488e3ab89d97461c4cbfd6fab4b6f83c29502914
Zachary <1734015+nopslip@users.noreply.github.com>
2021-10-21 20:21:53 -0600

    vite/vue3/tailwinds

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+
+<template>
+  <div>
+    <header class="bg-black shadow" v-if="$route.meta.title">
+      <div class="px-4 py-6 mx-auto max-w-7xl sm:px-6 lg:px-8"></div>
+    </header>
+    <main>
+      <router-view />
+    </main>
+  </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,30 @@
+<script>
+</script>
+
+<template>
+  <div class="grid grid-cols-1 bg-black px-4 py-8">
+    <div></div>
+    <div>
+      <div class="flex items-center justify-center">
+        <img
+          class="h-100 rounded-lg shadow-xl mb-6"
+          src="../assets/img/voltaire_dao_logo.jpg"
+          alt="Voltaire logo"
+        />
+      </div>
+      <div class="flex items-center justify-center">
+        <a
+          class="text-lg text-blue-300 font-bold"
+          target="_blank"
+          :href="'https://discord.gg/4ysPHjg2dP'"
+          >Enter the DAO</a
+        >
+      </div>
+      <div class="flex items-center justify-center py-40"></div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
+  
